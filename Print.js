76c34abc57fb5adb575358e2fad4cbce901143a9
Feranmi Onifade <105197655+FeronSam@users.noreply.github.com>
2022-06-25 14:29:02 +0100

    Update Print.js

--- Print.js
+++ Print.js
@@ -7,18 +7,14 @@ public class Myself
         System.out.print("Enter your name: ");
         String name = in.readLine();
         
-        System.out.print("Enter your Father's name: ");
-        String father = in.readLine();
+        System.out.print("Height: ");
+        String Height = in.readLine();
         
-        System.out.print("Enter your address: ");
-        String address = in.readLine();
-        
-        System.out.print("Enter your contact number: ");
-        long number = Long.parseLong(in.readLine());
-        
+        System.out.print("Country: ");
+        String country = in.readLine();
+              
         System.out.println("My name: " + name);
-        System.out.println("Father's name: " + father);
-        System.out.println("Postal address: " + address);
-        System.out.println("Contact Number: " + number);
+        System.out.println("Height: " + height);
+        System.out.println("Country: " + country);
     }
 }
